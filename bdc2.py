cc7ed73b526cfaaf6a667d748e8be30e3c6ae1cb
Jan Palach <jan.palach@icloud.com>
2019-12-30 17:05:21 -0300

    Update bdc2.py

--- bdc2.py
+++ bdc2.py
@@ -3,19 +3,18 @@ import json
 import psycopg2
 
 class DatabaseConnection:
-    def __init__(self):
-        try:
-            self.connection = psycopg2.connect(
-                host='xxxx'
-                , dbname= 'snowplow'       
-                , port= '5439'
-                , user= 'bruno_almeida'
-                , password= 'xxx')
-
-            self.connection.autocommit = True
-            self.cursor = self.connection.cursor()
-        except:
-            print("Cannot connect to database")
+    def __enter__(self):
+        # Aqui Bruno sempre que vc declarar um objeto da sua classe usando With isso vai ser executado, garantindo que na
+        # entrada vai ter uma conexão válida e um cursor pronto e válido.
+        with psycopg2.connect(host='xxxx', dbname= 'snowplow', port= '5439', user= 'bruno_almeida', password= 'xxx') as conn:
+            conn.autocommit = True
+            self.connection = conn
+            with self.connection.cursor() as curs:
+                self.cursor = curs
+     
+    def __exit__(self):
+        self.cursor.close()
+        self.connection.close()
 
     def execute_query(self):
         execute_query_command = ("SELECT id, app_codigo FROM pebmedapps.tb_usuario LIMIT 10;")
@@ -25,9 +24,6 @@ class DatabaseConnection:
         print (f"------ ----------- ")
         for row in rows:
             print (f"{row[0]}        {row[1]} ")
-        
-        self.cursor.close()
-        self.connection.close()
 
     def insert_record(self):
         new_record = ("10", "6")
@@ -37,5 +33,9 @@ class DatabaseConnection:
 
 
 if __name__ == '__main__':
-    db_connection = DatabaseConnection()
-    db_connection.execute_query()
+    # Quando entra no trecho identado no with é sinal de que seu enter executou com sucesso.
+    # ao sair do escopo aninhado do with, então o __exit__é chamado fechando cursor e conexão.
+    # verifica se seus execute e insert estão certinhos Bruno mas é basicamente isso para 
+    # garantir em Python que nada fica aberto.
+    with DatabaseConnection() as conn:
+        conn.execute_query()
